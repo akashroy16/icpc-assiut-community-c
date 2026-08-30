c88e2c93090bf3948daa3fd315b8d248f3ae608c
Akash Roy <akashavro4@gmail.com>
2026-08-31 01:24:39 +0600

    Add character case conversion logic
    Implement character case conversion in main function

---
+++ Sheet-1 (Data type - Conditions)/N - Char.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+ 
+int main(){
+char X;
+scanf("%c",&X);
+ 
+    if (X >= 'a' && X <= 'z') {
+        printf("%c",X-32);}
+       else if (X >= 'A' && X <= 'Z') {
+        printf("%c",X+32);}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;
+ 
+ 
+}
