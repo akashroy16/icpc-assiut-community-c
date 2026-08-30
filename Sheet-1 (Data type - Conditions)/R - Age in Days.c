fceb1937869e54c679f76bd4f4fa445fe5cd2e51
Akash Roy <akashavro4@gmail.com>
2026-08-31 01:27:01 +0600

    Add program to convert age in days to years and months

---
+++ Sheet-1 (Data type - Conditions)/R - Age in Days.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+    int N;
+    scanf("%d", &N);
+ 
+    int years = N / 365;
+    N = N % 365; 
+ 
+    int months = N / 30; 
+    N = N % 30; 
+ 
+   
+    printf("%d years\n", years);
+    printf("%d months\n", months);
+    printf("%d days\n", N);
+ 
+    return 0;
+}
