0eacc1de63da3c62a787095db44cd04ad9ea7bda
Akash Roy <akashavro4@gmail.com>
2026-08-31 01:32:49 +0600

    Add program to calculate last two digits of product

---
+++ Sheet-1 (Data type - Conditions)/Y - The last 2 digits.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    long long A, B, C, D;
+    scanf("%lld %lld %lld %lld", &A, &B, &C, &D);
+ 
+    long long result = (A % 100) * (B % 100) * (C % 100) * (D % 100);
+    result = result % 100;
+    printf("%02lld\n", result);
+ 
+    return 0;
+}
