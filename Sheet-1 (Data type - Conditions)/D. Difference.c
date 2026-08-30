e65cf82b2548c671767b5851029db3db13ff2902
akashroy16 <akashavro4@gmail.com>
2026-08-30 22:23:31 +0600

    Add solution for Problem D in Sheet 1

---
+++ Sheet-1 (Data type - Conditions)/D. Difference.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main()
+ {
+    long long A,B,C,D,E;
+    scanf("%lld%lld%lld%lld",&A,&B,&C,&D);
+    E=(A*B)-(C*D);
+ 
+    printf("Difference = %lld",E);
+ 
+ 
+ 
+    return 0;
+ 
+ }
