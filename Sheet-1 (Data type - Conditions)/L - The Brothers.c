f3753429a580fc13b6eeee260932bafbb4fa1ba9
Akash Roy <akashavro4@gmail.com>
2026-08-31 01:23:16 +0600

    Add program to check if two names are brothers

---
+++ Sheet-1 (Data type - Conditions)/L - The Brothers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main(){
+char a[500],b[500],c[500],d[500];
+    scanf("%s%s",&a,&b);
+    scanf("%s%s",&c,&d);
+    if (strcmp(b,d)==0){
+        printf("ARE Brothers");}
+        else{
+        printf("NOT");
+        }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;
+ 
+ 
+}
