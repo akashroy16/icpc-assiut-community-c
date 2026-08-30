8de15b2c253307cd923eb40b0b5a46df587a0605
Akash Roy <akashavro4@gmail.com>
2026-08-31 01:32:05 +0600

    Add C program for interval overlap detection
    Implement a program to find overlapping intervals.

---
+++ Sheet-1 (Data type - Conditions)/X - Two intervals.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int l1, r1, l2, r2;
+scanf("%d %d %d %d", &l1, &r1, &l2, &r2);
+int left = (l1 > l2) ? l1 : l2;
+int right = (r1 < r2) ? r1 : r2;
+if (left <= right) {
+printf("%d %d\n", left, right);
+}
+else {
+printf("-1\n"); 
+}
+return 0;
+}
