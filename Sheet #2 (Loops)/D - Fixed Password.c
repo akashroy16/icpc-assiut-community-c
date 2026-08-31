5898e6054d1a2d681473f08d1bfb3635bd3bfc86
Akash Roy <akashavro4@gmail.com>
2026-08-31 06:32:02 +0600

    Add password verification program

---
+++ Sheet #2 (Loops)/D - Fixed Password.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int A;
+    while(scanf("%d",&A)==1){
+    if(A==1999){
+        printf("Correct\n");
+        break;
+        
+    }
+    else{
+        printf("Wrong\n");
+    }
+    }
+    return 0;
+}
