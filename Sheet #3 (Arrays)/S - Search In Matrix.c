510a0cc5290f42fdc67fe222ee18390fcee83846
Akash Roy <akashavro4@gmail.com>
2026-09-01 14:51:17 +0600

    Add matrix search program in S - Search In Matrix.c
    Implement a matrix search program to find a number.

---
+++ Sheet #3 (Arrays)/S - Search In Matrix.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int main() {
+    int N, M, X, found = 0;
+    
+    scanf("%d %d", &N, &M);
+    int A[N][M];
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    
+    scanf("%d", &X);
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (A[i][j] == X) {
+                found = 1;
+                break;
+            }
+        }
+        if (found) break;
+    }
+    
+    if (found) {
+        printf("will not take number\n");
+    } else {
+        printf("will take number\n");
+    }
+    
+    return 0;
+}
