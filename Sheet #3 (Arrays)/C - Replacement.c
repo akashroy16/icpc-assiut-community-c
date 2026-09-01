6f9870cf10115bcc8387d2152a9d8517a7718d3b
Akash Roy <akashavro4@gmail.com>
2026-09-01 14:42:08 +0600

    Create C program for array replacement
    Implement a program that replaces negative numbers with 2 and positive numbers with 1 in an array.

---
+++ Sheet #3 (Arrays)/C - Replacement.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+    int N, index = -1; 
+    scanf("%d", &N);  
+    int A[N];  
+     
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);  
+    }
+   
+    for (int i = 0; i < N; i++) {
+        if (A[i]<0) {
+            A[i]=2;  
+        }
+        else if(A[i]>0){
+            A[i]=1;
+        }
+         printf("%d ", A[i]); 
+    }
+    
+   
+    return 0;
+}
