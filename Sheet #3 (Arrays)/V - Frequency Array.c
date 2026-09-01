fcb939be8e1acad623fd8eed459d2080cca6c11f
Akash Roy <akashavro4@gmail.com>
2026-09-01 14:52:48 +0600

    Add frequency array implementation in C

---
+++ Sheet #3 (Arrays)/V - Frequency Array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+    int N, M;
+    
+    scanf("%d %d", &N, &M);
+    
+    int A[N], freq[M + 1];
+    
+    for (int i = 0; i <= M; i++) {
+        freq[i] = 0;
+    }
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+        freq[A[i]]++;
+    }
+    
+    for (int i = 1; i <= M; i++) {
+        printf("%d\n", freq[i]);
+    }
+    
+    return 0;
+}
