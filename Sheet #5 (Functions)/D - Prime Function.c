a23bbd3819d11576b4ff3c8005dd8ebc2defd874
Akash Roy <akashavro4@gmail.com>
2026-09-04 23:51:52 +0600

    Add prime checking function and main logic

---
+++ Sheet #5 (Functions)/D - Prime Function.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int is_prime(int n) {
+    if (n <= 1) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+ 
+    int limit = sqrt(n);
+    for (int i = 3; i <= limit; i += 2) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+ 
+int main() {
+    int T;
+    scanf("%d", &T);
+ 
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+ 
+        if (is_prime(N)) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+ 
+    return 0;
+}
